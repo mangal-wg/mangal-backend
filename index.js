ddec6affc770a6c72c4b0b858c0258578b93428b
SteveViss <s.vissault@yahoo.fr>
2016-09-27 21:42:28 -0400

    Removing unexpected token;

--- index.js
+++ index.js
@@ -31,27 +31,27 @@ var baseapiurl = '/api/v0/'
 // Create REST resources
 var taxonResource = epilogue.resource({
   model: db.taxon,
-  endpoints: ['taxon','taxon/:id'].map(function(x){return(baseapiurl+x)});
+  endpoints: ['taxon','taxon/:id'].map(function(x){return(baseapiurl+x)})
 });
 
 var datasetResource = epilogue.resource({
   model: db.dataset,
-  endpoints: ['dataset','dataset/:id'].map(function(x){return(baseapiurl+x)});
+  endpoints: ['dataset','dataset/:id'].map(function(x){return(baseapiurl+x)})
 });
 
 var interactionResource = epilogue.resource({
   model: db.interaction,
-  endpoints: ['interaction','interaction/:id'].map(function(x){return(baseapiurl+x)});
+  endpoints: ['interaction','interaction/:id'].map(function(x){return(baseapiurl+x)})
 });
 
 var itemResource = epilogue.resource({
   model: db.item,
-  endpoints: ['item','item/:id'].map(function(x){return(baseapiurl+x)});
+  endpoints: ['item','item/:id'].map(function(x){return(baseapiurl+x)})
 });
 
 var networkResource = epilogue.resource({
   model: db.network,
-  endpoints: ['network','network/:id'].map(function(x){return(baseapiurl+x)});
+  endpoints: ['network','network/:id'].map(function(x){return(baseapiurl+x)})
 });
 
 // Sync database with new models
