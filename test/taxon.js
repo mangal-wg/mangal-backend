ba85c631bde2dbb0eaa5cf9c5433a0423cf02e78
Timothée Poisot <tim@poisotlab.io>
2016-09-27 21:04:11 -0400

    Additional tests

--- test/taxon.js
+++ test/taxon.js
@@ -2,12 +2,19 @@ var assert = require('assert');
 
 describe("Operations on taxa", function() {
     
-    describe("Adding a taxa", function() {
+    describe("Adding a taxon", function() {
 
-        it("should work if the taxa is unique");
-        it("should not work if the taxa is not unique");
-        it("should not work if the taxa has no name");
+        it("should work if the taxon is unique");
+        it("should not work if the taxon is not unique");
+        it("should not work if the taxon has no name");
 
     });
+
+    describe("GETting a taxon", function() {
+
+        it("should return 404 if there is no taxon with this ID");
+        it("should return a taxon with the correct ID if it exists");
+
+    })
 
 });
