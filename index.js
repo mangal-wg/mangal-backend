cd49d2860f3792b8076959490382f3bf3cf15bfc
SteveViss <s.vissault@yahoo.fr>
2016-09-29 17:30:52 -0400

    Fixing async issue with mocha

--- index.js
+++ index.js
@@ -12,20 +12,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Init ressources
 var api = require('./ressources').initialize(app);
 
-// test login authentification
-db.sequelize
-  .authenticate()
-  .then(function(success) {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(function (err) {
-    console.log('Unable to connect to the database:', err);
-  });
+// Start DB and server
+if(process.env.NODE_ENV == 'development') {
 
-// Sync database with new models
-db.sequelize.sync({force:true});
+  // test authentification
+  db.sequelize
+    .authenticate()
+    .then(function(success) {
+      console.log('Connection has been established successfully.');
+    })
+    .catch(function (err) {
+      console.log('Unable to connect to the database:', err);
+    });
+
+  // sync DB
+  db.sequelize.sync({force:true});
+
+};
 
 // start server
 var port = process.env.PORT || 3000;
 server = http.createServer(app);
 server.listen(port);
+
+module.exports = server;
--- test/taxon.js
+++ test/taxon.js
@@ -1,52 +1,43 @@
 var fs = require('fs');
 var db = require('../models');
 var should = require('should');
-var supertest = require('supertest');
+var request = require('supertest');
 
-// Set Test environment
-process.env.NODE_ENV = 'test'
-var app = require('../')
-var server = supertest.agent("http://localhost:3000");
+// Set app test
+var server = require('../')
 
-describe("Operations on taxa", function() {
+beforeEach(function(done) {
+    db.sequelize.sync({
+        force: true
+    }).then(function() {
+        done();
+    });
+});
 
-    // beforeEach(function(){
-    //   return db.sequelize.sync({
-    //     force:true,
-    //     logging: false
-    //   });
-    // });
+describe("Adding a taxon", function() {
 
-    describe("Adding a taxon", function() {
+    it("should work if the taxon is unique", function(done){
 
-        it("should work if the taxon is unique", function(done) {
-
-            var file = './test/data/taxon/vulpes_vulpes.json'
-            var data = JSON.parse(fs.readFileSync(file, 'utf8'));
-
-            server
-                .post('/api/v0/taxon')
-                .send(data)
-                .end(function(err, res) {
-                    console.log(res);
-                    if (err) {
-                        throw err;
-                    }
-                    res.status.should.be.equal(201);
-                    done();
-                });
-        });
-
-        it("should not work if the taxon is not unique");
-        it("should not work if the taxon has no name");
-
+      var file = './test/data/taxon/vulpes_vulpes.json'
+      var data = JSON.parse(fs.readFileSync(file, 'utf8'));
+      
+        request('http://localhost:3000')
+            .post('/api/v0/taxon')
+            .send(data)
+            .end(function(err, res) {
+              if (err) return done(err);
+              done();
+            });
     });
 
-    describe("GETting a taxon", function() {
-
-        it("should return 404 if there is no taxon with this ID");
-        it("should return a taxon with the correct ID if it exists");
-
-    })
+    it("should not work if the taxon is not unique");
+    it("should not work if the taxon has no name");
 
 });
+
+describe("GETting a taxon", function() {
+
+    it("should return 404 if there is no taxon with this ID");
+    it("should return a taxon with the correct ID if it exists");
+
+})
