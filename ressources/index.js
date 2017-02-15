64a1e2f433597390be3b6db193975595bf23d35b
Eltisho <vincent.bellavance@USherbrooke.ca>
2017-02-15 16:07:59 -0500

    add all the endpoints

--- ressources/index.js
+++ ressources/index.js
@@ -10,24 +10,49 @@ var initialize = function(app) {
   });
 
   // Create REST resources
+  var attributeResource = epilogue.resource({
+    model: db.attribute,
+    endpoints: ['/api/v0/attributes','/api/v0/attribute/:id']
+  });
+
+  var refResource = epilogue.resource({
+    model: db.ref,
+    endpoints: ['/api/v0/refs','/api/v0/ref/:id']
+  });
+
+  var userResource = epilogue.resource({
+    model: db.user,
+    endpoints: ['/api/v0/users','/api/v0/user/:id']
+  });
+
+  var traitResource = epilogue.resource({
+    model: db.trait,
+    endpoints: ['/api/v0/traits','/api/v0/trait/:id']
+  });
+
+  var environmentResource = epilogue.resource({
+    model: db.environment,
+    endpoints: ['/api/v0/environments','/api/v0/environment/:id']
+  });
+
   var taxonResource = epilogue.resource({
     model: db.taxon,
-    endpoints: ['/api/v0/taxon','/api/v0/taxon/:id']
+    endpoints: ['/api/v0/taxons','/api/v0/taxon/:id']
   });
 
   var datasetResource = epilogue.resource({
     model: db.dataset,
-    endpoints: ['/api/v0/dataset','/api/v0/dataset/:id']
-  });
-
-  var interactionResource = epilogue.resource({
-    model: db.interaction,
-    endpoints: ['/api/v0/interaction','/api/v0/interaction/:id']
+    endpoints: ['/api/v0/datasets','/api/v0/dataset/:id']
   });
 
   var networkResource = epilogue.resource({
     model: db.network,
-    endpoints: ['/api/v0/network','/api/v0/network/:id']
+    endpoints: ['/api/v0/networks','/api/v0/network/:id']
+  });
+
+  var interactionResource = epilogue.resource({
+    model: db.interaction,
+    endpoints: ['/api/v0/interactions','/api/v0/interaction/:id']
   });
 
 
