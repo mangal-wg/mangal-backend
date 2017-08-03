302a0f5eb5dffd8f03cf4b713fb0944c0d11f8c2
Steve Vissault <s.vissault@yahoo.fr>
2017-08-03 16:23:53 -0400

    User model in mangal-api overwrites orcid-oauth2 user model

--- models/user.js
+++ models/user.js
@@ -1,31 +1,34 @@
 "use strict";
 
 module.exports = function(sequelize, DataTypes) {
-    var user = sequelize.define('user', {
-        name: {
-            type: DataTypes.STRING
-        },
-        email: {
-            type: DataTypes.STRING
-        },
-        orcid: {
-            type: DataTypes.STRING
-        },
-        organization: {
-            type: DataTypes.STRING
-        },
-        type: {
-            type: DataTypes.ENUM,
-            values: [
-                "user",
-                "curator",
-                "administrator"
-            ],
-            defaultValue: "user",
-            allowNull: false,
-            comment: "The status of the user. Can be user, curator, or administrator."
-        }
-    }, {
+  var user = sequelize.define('user', {
+      name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true
+      },
+      orcid: {
+          type: DataTypes.STRING,
+          unique: true
+      },
+      organization: {
+          type: DataTypes.STRING
+      },
+      access_token: {
+          type: DataTypes.STRING
+      },
+      type: {
+          type: DataTypes.ENUM,
+          values: [
+              "user",
+              "curator",
+              "administrator"
+          ],
+          defaultValue: "user",
+          allowNull: false,
+          comment: "The status of the user. Can be user, curator, or administrator."
+      }
+  }, {
         underscored: true,
         classMethods: {
             associate: function(models) {
