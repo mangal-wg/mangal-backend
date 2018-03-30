2fab79cef3d4957fc30d96c8471dbf36e0eaca56
Steve Vissault <s.vissault@yahoo.fr>
2018-03-30 13:59:55 -0400

    transform to v2

--- ressources/index.js
+++ ressources/index.js
@@ -12,52 +12,52 @@ var initialize = function(app) {
   // Create REST resources
   var attributeResource = epilogue.resource({
     model: db.attribute,
-    endpoints: ['/api/v0/attribute', '/api/v0/attribute/:id']
+    endpoints: ['/api/v2/attribute', '/api/v2/attribute/:id']
   });
 
   var refResource = epilogue.resource({
     model: db.ref,
-    endpoints: ['/api/v0/ref', '/api/v0/ref/:id']
+    endpoints: ['/api/v2/ref', '/api/v2/ref/:id']
   });
 
   var userResource = epilogue.resource({
     model: db.user,
-    endpoints: ['/api/v0/users', '/api/v0/user/:id']
+    endpoints: ['/api/v2/users', '/api/v2/user/:id']
   });
 
   var traitResource = epilogue.resource({
     model: db.trait,
-    endpoints: ['/api/v0/trait', '/api/v0/trait/:id']
+    endpoints: ['/api/v2/trait', '/api/v2/trait/:id']
   });
 
   var environmentResource = epilogue.resource({
     model: db.environment,
-    endpoints: ['/api/v0/environment', '/api/v0/environment/:id']
+    endpoints: ['/api/v2/environment', '/api/v2/environment/:id']
   });
 
   var taxonResource = epilogue.resource({
     model: db.taxon,
-    endpoints: ['/api/v0/taxa', '/api/v0/taxa/:id']
+    endpoints: ['/api/v2/taxa', '/api/v2/taxa/:id']
   });
 
   var datasetResource = epilogue.resource({
     model: db.dataset,
-    endpoints: ['/api/v0/dataset', '/api/v0/dataset/:id']
+    endpoints: ['/api/v2/dataset', '/api/v2/dataset/:id']
   });
 
   var networkResource = epilogue.resource({
     model: db.network,
-    endpoints: ['/api/v0/network', '/api/v0/network/:id']
+    endpoints: ['/api/v2/network', '/api/v2/network/:id']
   });
 
   var interactionResource = epilogue.resource({
     model: db.interaction,
-    endpoints: ['/api/v0/interaction', '/api/v0/interaction/:id']
+    endpoints: ['/api/v2/interaction', '/api/v2/interaction/:id']
   });
 
   var taxaBackResource = epilogue.resource({
     model: db.taxo_back,
-    endpoints: ['/api/v0/taxa_back', '/api/v0/taxa_back/:id']
+    endpoints: ['/api/v2/taxa_back', '/api/v2/taxa_back/:id']
   });
 
   // Protections ressources with middlewares
